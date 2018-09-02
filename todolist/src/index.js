71740330960896c440d192594d66dc7a9c389c8b
unveiledrain <umerhasan17@gmail.com>
2018-09-02 14:07:17 +0100

    No such dir bug

---
+++ todolist/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <p>Hello World</p>
+    </div> , destination
+);
