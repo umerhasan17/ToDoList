56934a87783cceab40f702bb0cc00fc8235796c2
unveiledrain <umerhasan17@gmail.com>
2018-09-02 14:11:40 +0100

    Creating simple TodoList component

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import TodoList from "./TodoList";
 
 var destination = document.querySelector("#container");
 
 ReactDOM.render(
     <div>
-        <p>Hello World</p>
+        <TodoList/>
     </div> , destination
 );
