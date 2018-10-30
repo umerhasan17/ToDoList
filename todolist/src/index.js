fd06171c3ba10622f77df0f803fa9bef0b72944c
umerhasan17 <umerhasan17@gmail.com>
2018-10-30 10:04:57 +0000

    Updating container import

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import TodoList from "./TodoList";
+import TodoList from "./toDoListContainer.js";
 
 var destination = document.querySelector("#container");
 
